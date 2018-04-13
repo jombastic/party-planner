8169813c89f8c4313121780c097f929619fdc5da
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-13 14:58:13 +0200

    Add setDrinkType method that sets the drinks and returns false if wrong input

--- src/main/java/models/EventPlanner.java
+++ src/main/java/models/EventPlanner.java
@@ -3,16 +3,18 @@ package models;
 public class EventPlanner {
   private int guests;
   private String drinkType;
+  private String[] drinks = {"non-alcohol", "alcohol", "full bar"};
   private String food;
   private String entertainment;
+  private String location;
 
-  public EventPlanner(int guests, String drinkType, String food, String entertainment, String location) {
+  /*public EventPlanner(int guests, String drinkType, String food, String entertainment, String location) {
     this.guests = guests;
     this.drinkType = drinkType;
     this.food = food;
     this.entertainment = entertainment;
     this.location = location;
-  }
+  }*/
 
   public int getGuests() {
     return guests;
@@ -34,7 +36,20 @@ public class EventPlanner {
     return location;
   }
 
-  private String location;
+  public void setGuests(int guests) {
+    this.guests = guests;
+  }
 
-
+  public boolean setDrinkType(String drinkType) {
+    for (int i = 0; i < drinks.length; i++) {
+      if (drinkType.equals(drinks[i])) {
+        return true;
+      } else {
+        this.drinkType = drinkType;
+      }
+    }
+    System.out.println("Beverage type not recognised. Please enter from the following:" +
+    String.join(", ", drinks));
+    return false;
+  }
 }
--- src/test/java/models/EventPlannerTest.java
+++ src/test/java/models/EventPlannerTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EventPlannerTest {
-  @Test
+  /*@Test
   public void EventPlanner_instantiatesProperly() {
     EventPlanner testPlanner = new EventPlanner(10, "full bar", "italian", "dj", "hotel");
     assertEquals(true, testPlanner instanceof EventPlanner);
+  }*/
+
+  @Test
+  public void setDrinkType_checkIfDrinkIsAnOption_true() {
+    EventPlanner testPlanner = new EventPlanner();
+    assertEquals(true, testPlanner.setDrinkType("alcohol"));
   }
 }
