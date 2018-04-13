0ae4a03c14a673c92a529d1a9b957f81b137c58a
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-13 23:32:31 +0200

    Add front-end logic that displays the receipt and asks the user if he wants to plan another party

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import models.EventPlanner;
 
 public class App {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     //String beverages;
-    EventPlanner newPlan = new EventPlanner(0,null, null,null,null);
+    EventPlanner newPlan = new EventPlanner(0, null, null, null, null);
     System.out.println("Welcome to the ultimate party planer!");
     System.out.println("Select from our offers and enjoy the parties of a lifetime");
-    boolean chooseVenues = true;
+    boolean chooseVenues;
+    int guests = 0;
     do {
-      System.out.println("Enter the number of guests you expect: ");
-      String numberString = scanner.nextLine();
-      while (!isNumber(numberString)) {
-        System.out.println("Wrong input");
-        numberString = scanner.nextLine();
+      while (guests <= 0) {
+        System.out.println("\nEnter the number of guests you expect: ");
+        String numberString = scanner.nextLine();
+        while (!isNumber(numberString)) {
+          System.out.println("Wrong input");
+          numberString = scanner.nextLine();
+        }
+        guests = Integer.parseInt(numberString);
+        newPlan.setGuests(guests);
       }
 
       System.out.println("Choose beverage preferences: non-alcohol, alcohol or full bar.");
@@ -41,6 +47,24 @@ public class App {
       String location = scanner.nextLine();
       while (!newPlan.setLocation(location)) {
         location = scanner.nextLine();
+      }
+
+      System.out.println("Your receipt:");
+      System.out.println("----------------------------");
+      System.out.println(newPlan.getReceipt());
+      System.out.println("----------------------------");
+      System.out.println("The full cost of all venues is: " + newPlan.getFullCost());
+
+      String anotherParty;
+      do {
+        System.out.println("\nPlan another party? Enter y or n");
+        anotherParty = scanner.nextLine();
+      } while ((!anotherParty.equalsIgnoreCase("y")) && (!anotherParty.equalsIgnoreCase("n")));
+
+      if (anotherParty.equalsIgnoreCase("y")) {
+        chooseVenues = true;
+      } else {
+        chooseVenues = false;
       }
     } while (chooseVenues);
   }
